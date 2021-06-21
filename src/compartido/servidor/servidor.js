0ce43f0aed51e58b6a661b50cfb37f8519c3fe28
eerjoker <rivera.esteban1994@gmail.com>
2021-06-21 15:20:20 -0300

    factory servidor importa los routers

--- src/compartido/servidor/servidor.js
+++ src/compartido/servidor/servidor.js
@@ -1,16 +1,13 @@
-import { crearRouterRegistro } from "../../usuarios/ruteo/registroRouter.js";
-import { crearRouterReserva } from "../../reservas/ruteo/reservaRouter.js";
-import { crearRouterRecordatorios } from "../../reservas/ruteo/recordatoriosRouter.js";
 import express from "express";
 
-function crearServidor() {
+function crearServidor(routers) {
   const app = express();
 
   app.use(express.json());
 
-  app.use('/usuarios', crearRouterRegistro())
-     .use('/reserva', crearRouterReserva())
-     .use('/recordatorios', crearRouterRecordatorios())
+  app.use('/usuarios', routers.routerUsuarios)
+     .use('/reserva', routers.routerReservas)
+     .use('/recordatorios', routers.routerRecordatorios)
 
   let server = null
 
--- src/compartido/servidor/servidorFactory.js
+++ src/compartido/servidor/servidorFactory.js
@@ -1,7 +1,18 @@
+import { crearRouterRegistro } from "../../usuarios/ruteo/registroRouter.js";
+import { crearRouterReserva } from "../../reservas/ruteo/reservaRouter.js";
+import { crearRouterRecordatorios } from "../../reservas/ruteo/recordatoriosRouter.js";
 import {crearServidor} from './servidor.js'
 
 function crearServidorFactory(port = 8080) {
-  const servidor = crearServidor()
+  const routerUsuarios = crearRouterRegistro()
+  const routerReservas = crearRouterReserva()
+  const routerRecordatorios = crearRouterRecordatorios()
+
+  const servidor = crearServidor({ 
+    routerUsuarios,
+    routerReservas,
+    routerRecordatorios
+  })
   
   return {
     conectarServidor: async () => {
