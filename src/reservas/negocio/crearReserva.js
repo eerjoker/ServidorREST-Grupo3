4892e64881c7578bf67f3dea2020976ccc833f35
Antonella Castellano <antonellacastellano97@gmail.com>
2021-06-22 00:49:25 -0300

    manejo de error en crearReserva

--- src/reservas/negocio/crearReserva.js
+++ src/reservas/negocio/crearReserva.js
@@ -2,11 +2,15 @@
 
 import Reserva from "../modelos/Reserva.js";
 import generarPDF from "../../compartido/services/generadorPDF/generarPDF.js";
+import { crearErrorDeReservaVacia }  from "../../compartido/errors/errorDeReservaVacia.js";
 
 function crearCuCrearReserva(daoReserva, mailer) {
   return {
     async ejecutar(datos) {
-      const reserva = new Reserva(datos);
+          const reserva = new Reserva(datos);
+          if (datos==null) {
+        throw crearErrorReservaVacia("los datos de la reserva estan vacios");
+      }
       await daoReserva.guardar(reserva);
       await generarPDF(reserva);
       await mailer.enviarConAdjunto(
