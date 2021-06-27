bc5f61b07a272177416638a62842df63909dc849
ivanri <ivan.riboldi@outlook.com>
2021-06-26 22:58:14 -0300

    se configura la variable de entorno de la api key

--- src/clima/negocio/CUFactory.js
+++ src/clima/negocio/CUFactory.js
@@ -1,12 +1,13 @@
-import ConsultarClima from './CUConsultarClima.js'
-import Pronosticador from '../../compartido/services/clima/pronosticador.js'
+import ConsultarClima from "./CUConsultarClima.js";
+import Pronosticador from "../../compartido/services/clima/pronosticador.js";
+import { getClimaApiKey } from "../../compartido/config.js";
 
 function crearCU_ConsultarClima() {
-    const pronosticador = new Pronosticador('1b50acfccfccab2eb23d9240cb6e8d21')
-    const CU_Clima = ConsultarClima(pronosticador)
-    return CU_Clima
+  const pronosticador = new Pronosticador(getClimaApiKey());
+  const CU_Clima = ConsultarClima(pronosticador);
+  return CU_Clima;
 }
 
 export default {
-    crearCU_ConsultarClima
-}
+  crearCU_ConsultarClima,
+};
--- src/compartido/config.js
+++ src/compartido/config.js
@@ -1,21 +1,24 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const getCnxMail = () => {
-  
-    return {
-        mail: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
-    }
-} 
+  return {
+    mail: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  };
+};
 
 function getCnxStr() {
-    return process.env.CNX_STR
-  }
-  
-  function getMode() {
-    return process.env.MODE || 'TEST'
-  }
+  return process.env.CNX_STR;
+}
 
-export {getCnxMail, getCnxStr, getMode}
+function getMode() {
+  return process.env.MODE || "TEST";
+}
+
+function getClimaApiKey() {
+  return process.env.CLIMA_API_KEY;
+}
+
+export { getCnxMail, getCnxStr, getMode, getClimaApiKey };
