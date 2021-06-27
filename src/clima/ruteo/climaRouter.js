d0f41084601146f1605634a794f79ae40ed451c0
ivanri <ivan.riboldi@outlook.com>
2021-06-26 22:41:36 -0300

    se agregan valores a la ruta por query

--- src/clima/ruteo/climaRouter.js
+++ src/clima/ruteo/climaRouter.js
@@ -1,24 +1,26 @@
-import express from 'express'
-import CUFactory from '../negocio/CUFactory.js'
+import express from "express";
+import CUFactory from "../negocio/CUFactory.js";
 
-const router = express.Router()
-const CasoDeUso_ConsultarClima = CUFactory.crearCU_ConsultarClima()
+const router = express.Router();
+const CasoDeUso_ConsultarClima = CUFactory.crearCU_ConsultarClima();
 
-const crearClimaRoute = ()=>{
-
-  router.get('/:date', async (req, res) => {
-    const {date} = req.params;
-    try{
-      const climaData = await CasoDeUso_ConsultarClima.ejecutar(date, -36.0, -60.0)
-      const msg = climaData ? 'ok' : 'No encontramos datos para esa fecha'
-      res.json({ msg, clima: climaData || null  })
-    } catch (e){
-      res.status(400).json({msg: e.message})
+const crearClimaRoute = () => {
+  router.get("/", async (req, res) => {
+    const { date, lat, long } = req.query;
+    try {
+      const climaData = await CasoDeUso_ConsultarClima.ejecutar(
+        date,
+        lat,
+        long
+      );
+      const msg = climaData ? "ok" : "No encontramos datos para esa fecha";
+      res.json({ msg, clima: climaData || null });
+    } catch (e) {
+      res.status(400).json({ msg: e.message });
     }
-  })
+  });
 
-  return router
-}
+  return router;
+};
 
-
-export { crearClimaRoute }
+export { crearClimaRoute };
--- tests/clima/clima.test.js
+++ tests/clima/clima.test.js
@@ -1,39 +1,46 @@
-import { crearServidorFactory } from "../../src/compartido/servidor/servidorFactory.js"
-import axios from 'axios'
+import { crearServidorFactory } from "../../src/compartido/servidor/servidorFactory.js";
+import axios from "axios";
 
 async function main() {
+  const servidor = crearServidorFactory().crearServidor();
 
-    const servidor = crearServidorFactory().crearServidor();
+  await servidor.conectar(3000);
 
-    await servidor.conectar(3000)
+  const date = new Date(2021, 5, 25);
+  const latitud = -36.0;
+  const longitud = -60.0;
+  const dateFail = "asd";
+  const dateOutOfRange = new Date(2021, 6, 25);
 
-    const date = new Date(2021,5,25)
-    const dateFail = 'asd'
-    const dateOutOfRange = new Date(2021,6,25)
+  console.log("Clima test exitoso");
+  try {
+    const { data } = await axios.get(
+      `http://localhost:${3000}/clima?date=${date}&lat=${latitud}&long=${longitud}`
+    );
+    console.log("REST SERVER RESULT: ", data);
+  } catch (err) {
+    console.log("ERROR", err.response.data);
+  }
 
-    console.log("Clima test exitoso")
-    try{
-        const {data} = await axios.get(`http://localhost:${3000}/clima/${date}`)
-        console.log("REST SERVER RESULT: ", data)
-    } catch (err){
-        console.log("ERROR", err.response.data)
-    }
+  console.log("Clima test fallido");
+  try {
+    const { data } = await axios.get(
+      `http://localhost:${3000}/clima?date=${dateFail}&lat=${latitud}&long=${longitud}`
+    );
+    console.log("REST SERVER RESULT: ", data);
+  } catch (err) {
+    console.log("ERROR", err.response.data);
+  }
 
-    console.log("Clima test fallido")
-    try{
-        const {data} = await axios.get(`http://localhost:${3000}/clima/${dateFail}`)
-        console.log("REST SERVER RESULT: ", data)
-    } catch (err){
-        console.log("ERROR", err.response.data)
-    }
-
-    console.log("Clima test fuera de rango")
-    try{
-        const {data} = await axios.get(`http://localhost:${3000}/clima/${dateOutOfRange}`)
-        console.log("REST SERVER RESULT: ", data)
-    } catch (err){
-        console.log("ERROR", err.response.data)
-    }
+  console.log("Clima test fuera de rango");
+  try {
+    const { data } = await axios.get(
+      `http://localhost:${3000}/clima?date=${dateOutOfRange}&lat=${latitud}&long=${longitud}`
+    );
+    console.log("REST SERVER RESULT: ", data);
+  } catch (err) {
+    console.log("ERROR", err.response.data);
+  }
 }
 
-main()
+main();
